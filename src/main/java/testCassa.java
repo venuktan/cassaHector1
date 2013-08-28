a03c658259db12f887dc823bd21fc03b2c65391f
Venu K Tangirala <venuktan@gmail.com>
2013-08-27 17:32:49 -0700

    First commit

---
+++ src/main/java/testCassa.java
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: venuktangirala
+ * Date: 7/22/13
+ * Time: 11:40 AM
+ * To change this template use File | Settings | File Templates.
+ */
+import me.prettyprint.hector.api.*;
+import me.prettyprint.hector.api.factory.HFactory;
+
+public class testCassa {
+    Cluster myCluster = HFactory.getOrCreateCluster("test-cluster", "localhost:9160");
+    sdfsdfdsa
+
+
+}
